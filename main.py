b0f64d14266c28ca6b5f7bc272ade47adaf3cc6e
Turn-around-towards-the-sea <66402506+Turn-around-towards-the-sea@users.noreply.github.com>
2022-05-29 12:15:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests, time, datetime, re,sys, json, random
 # 用户名（格式为 13800138000）
 
 # 酷推skey和server酱sckey和企业微信设置，只用填一个其它留空即可
-skey = sys.argv[3]
+skey = sys.argv[]
 # 推送server酱
 sckey = sys.argv[4]
 # 企业微信推送
